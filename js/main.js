602ce4e489ae350b4b6bdc3d62c48f8fb723468d
KELbTIK <jonhitman@mail.ru>
2015-05-29 00:55:28 +0300

    mobile version

--- js/main.js
+++ js/main.js
@@ -165,7 +165,6 @@ $(window).load(function(){
 
     $(".main").onepage_scroll({
         sectionContainer: "section",
-        responsiveFallback: 600,
         loop: true
     });
     slider_4 = $('#slider-4').bxSlider();
